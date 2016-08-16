8b4c9d55b08bdf358226e6ea50a87b32fcaa1c1d
Johan Persson <johan.persson@schibsted.se>
2016-08-16 15:52:29 +0200

    Added the build that does the actual building.

---
+++ packages/roc-plugin-react-hot-ts/src/builder/index.js
@@ -0,0 +1,49 @@
+import path from 'path';
+import { getSettings } from 'roc';
+
+
+export default () => ({ previousValue: rocBuilder }) => () => () => {
+    const {
+        buildConfig,
+        builder,
+        info
+    } = rocBuilder;
+
+    const buildSettings = getSettings('build');
+    const DEV = (buildSettings.mode === 'dev');
+
+    if (DEV) {
+        // Add hot reloading capabilities for tsx-files
+        const reactHotLoader = {
+            test: /\.tsx?$/,
+            loader: 'react-hot'
+        };
+
+        // Check if ts-loader has already been added to the loaders
+        let tsLoaderIndex = -1;
+        buildConfig.module.loaders.find((loader, index) => {
+            if (loader.loader === 'ts-loader') {
+                tsLoaderIndex = index;
+                return true;
+            }
+
+            return false;
+        });
+
+        // If ts-loader has been added, add react-hot before ts-loader, or it won't work
+        if (tsLoaderIndex >= 0) {
+            buildConfig.module.loaders.splice(tsLoaderIndex, 0, reactHotLoader);
+        } else {
+            buildConfig.module.loaders.push(reactHotLoader);
+        }
+
+        const modulesPath = path.join(__dirname, '../../node_modules');
+        rocBuilder.buildConfig.resolveLoader.root.push(modulesPath);
+    }
+
+    return {
+        buildConfig,
+        builder,
+        info
+    };
+};
--- packages/roc-plugin-react-hot-ts/src/roc/index.js
+++ packages/roc-plugin-react-hot-ts/src/roc/index.js
@@ -3,8 +3,17 @@ import meta from '../config/roc.config.meta.js';
 
 import { name } from './util';
 
+import builder from './builder';
+
 export default {
     name,
     config,
-    meta
+    meta,
+    actions: {
+        reactHotTs: {
+            hook: 'build-webpack',
+            description: 'Adds hot reload support via webpacks HMR to projects using react and typescript.',
+            action: builder
+        }
+    }
 };
